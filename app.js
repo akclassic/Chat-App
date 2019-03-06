13a4e33ead4d3f85980eab42b618813179fa7986
akclassic <ankitjh521@gmail.com>
2019-03-06 19:04:53 +0530

    files added

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const port = process.env.Port || 5000;
+
+server.listen(port, () => {
+    console.log(`listening to server on port ${port}`);
+});
+
+//set public folder
+app.use('/', express.static('public'));
+
+let userinfo = [];
+
+//on connecting a user 
+io.on('connection', function (socket) {
+    //event username
+    socket.on('username', (username) => {
+        let info = {
+            'name': username,
+            'id': socket.id
+        };
+        userinfo.push(info);
+        console.log(userinfo);
+        socket.broadcast.emit('user connected', userinfo[userinfo.length-1]);
+    });
+
+    //event chat message
+    socket.on('chat message', function (info) {
+        io.emit('chat message', info.username, info.msg);
+    });
+
+    //event disconnect
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user disconnected', userinfo[userinfo.length-1]);
+    });
+});
